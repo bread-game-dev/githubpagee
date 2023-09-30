fa7005917cca8459c5194f5239e8e8838624679e
shakir428 <shakiregal19@gmail.com>
2023-09-30 16:12:14 +0200

    falier

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,14 @@ function App() {
 
   return (
    <>
-   
+
+   <h1>video player </h1>
+   <br />
+   <video controls src="./video.mp4">does not <br /> support video tag </video>
+
+
+   <button type="submit">follow</button>
+   <button>like</button>
    </>
   )
 }
