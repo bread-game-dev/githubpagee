5cca82b4be12224816b6ff38ca550cd257472b37
shakir428 <shakiregal19@gmail.com>
2023-09-30 15:48:05 +0200

    file deploy.yml added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/shakir482.github.io",
+  base: "/githubpagee/",
 
 })
